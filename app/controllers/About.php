9cb3f25475c4a242535c6462194fea730e065fde
fikrifrramdani <fikriramdani32@gmail.com>
2019-10-08 11:25:37 +0700

    menambahkan file statis

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -8,6 +8,7 @@ class About extends Controller
 		$data['pekerjaan'] = $pekerjaan;
 		$data['umur'] = $umur;
 		$data['judul'] = 'About Me';
+
 		$this->view('templates/header', $data);
 		$this->view('about/index', $data);
 		$this->view('templates/footer');
